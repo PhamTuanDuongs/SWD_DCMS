214ed1e26c76d868fcf6ff6f224b759784903635
Loc Do <doloc181103@gmail.com>
2024-03-18 01:00:11 +0700

    fix: login be

--- swd/src/main/java/dcms/be/swd/controller/AuthenticationController.java
+++ swd/src/main/java/dcms/be/swd/controller/AuthenticationController.java
@@ -6,9 +6,11 @@ import dcms.be.swd.service.AccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthenticationController {
 
     private AccountService accountService;
