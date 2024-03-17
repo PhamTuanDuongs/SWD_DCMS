54ad6822417870794deed2b1076adf833f7fc681
linhlt <linhlthe171271@fpt.edu.vn>
2024-03-18 01:57:27 +0700

    feat: add employee

--- swd/src/main/java/dcms/be/swd/repository/UserRepository.java
+++ swd/src/main/java/dcms/be/swd/repository/UserRepository.java
@@ -1,30 +1,27 @@
 package dcms.be.swd.repository;
 
+
 import dcms.be.swd.dto.User.UserProfileResponse;
 import dcms.be.swd.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 
+import org.springframework.stereotype.Repository;
+
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Query(value = """
-            select u.name as name, 
-                   u.dob as dob, 
-                   u.address as address, 
-                   u.phoneNo as phoneNo, 
-                   u.gender as gender, 
-                   u.nationalId as nationalId, 
-                   u.avatar as avatar,
-                   a.email as email, 
-                   a.createdAt as createdAt, 
-                   r.roleName as roleName 
-            from User u 
-            join u.userAccounts a 
-            join a.role r 
-            where u.id = :id
-                """)
+
+    @Query(value = "select u.name as name, u.dob as dob,u.address as address,  u.phoneNo as phoneNo, u.gender as gender, u.nationalId as nationalId, u.avatar as avatar, a.email as email, a.createdAt as createdAt, r.roleName as roleName from User u join u.userAccounts a join a.role r where u.id = :id ")
     UserProfileResponse getUserProfileById(int id);
 
     User getUserById(int id);
+
+    @Query(value="SELECT * FROM User u", nativeQuery=true)
+    Page<User> findAllEmployeePaginate(Pageable paging);
+
 }
