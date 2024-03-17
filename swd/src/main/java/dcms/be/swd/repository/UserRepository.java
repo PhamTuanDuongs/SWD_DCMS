559ecc49e0f35b500e3346731b413d533778b340
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2024-03-18 02:03:16 +0700

    get medstaff info

--- swd/src/main/java/dcms/be/swd/repository/UserRepository.java
+++ swd/src/main/java/dcms/be/swd/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package dcms.be.swd.repository;
 
 import dcms.be.swd.dto.User.UserProfileResponse;
+import dcms.be.swd.dto.medstaff.MedStaffResponse;
 import dcms.be.swd.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -25,6 +26,22 @@ public interface UserRepository extends JpaRepository<User, Integer> {
             where u.id = :id
                 """)
     UserProfileResponse getUserProfileById(int id);
+
+    @Query(value = """
+            select u.name as name, 
+                   u.dob as dob, 
+                   u.address as address, 
+                   u.phoneNo as phoneNo, 
+                   u.gender as gender, 
+                   u.nationalId as nationalId, 
+                   u.avatar as avatar,
+                   m.quanlification as quanlification,
+                    m.experience as experience
+            from User u 
+            join u.userMedStaffInfoes m
+            where u.id = :id
+                """)
+    MedStaffResponse getMedStaffById(int id);
 
     User getUserById(int id);
 }
